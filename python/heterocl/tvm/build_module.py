56864900e1b434cd3e662cf08d8693c8175fd487
chhzh123 <982270930@qq.com>
2020-05-16 15:57:05 +0800

    [Runtime] Add Vivado HLS report extraction script

--- python/heterocl/tvm/build_module.py
+++ python/heterocl/tvm/build_module.py
@@ -48,6 +48,25 @@ def tvm_callback_exec_evaluate(platform, mode, host_only):
       out = run_process("cd project; make vivado 2>&1")
       print(out)
 
+      def extract_rpt():
+        res = ""
+        rptfile = os.path.join("project", "out.prj", "solution1/syn/report/test_csynth.rpt")
+        with open(rptfile, "r") as rpt:
+            rpt = list(map(str.lstrip,list(rpt)))
+            for i, line in enumerate(rpt):
+                if "Timing (ns)" in line:
+                    res += "* Timing (ns)\n  "
+                    res += '  '.join(rpt[i+2:i+7])
+                elif "Latency (clock cycles)" in line:
+                    res += "* Latency (clock cycles)\n  "
+                    res += '  '.join(rpt[i+2:i+8])
+                elif "Utilization Estimates" in line:
+                    res += "* Utilization Estimates\n  "
+                    res += '  '.join(rpt[i+3:i+20])
+        return res
+
+      print(extract_rpt())
+
     elif platform == "vivado_hls": 
 
       assert os.system("which vivado_hls >> /dev/null") == 0, \
