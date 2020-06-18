1fdafed5ca4f17c0ecfaa192bdecbd5e3bf47b3e
ChenHZ <chenhzh37@mail2.sysu.edu.cn>
2020-06-19 01:39:08 +0800

    [profile] Add multiple schedules

--- python/heterocl/profiler.py
+++ python/heterocl/profiler.py
@@ -19,26 +19,35 @@ class Profiler():
         self.bandwidth_roof = 1600 * 1000 * 1000 * 8 # B/s
         self.compute_roof = 276 * 1000 * 1000 * 1000 # FLOP/s
         self.ridge_point = self.compute_roof / self.bandwidth_roof
+        self.initialize_perf()
 
     def clear(self):
         self.perf = {}
+        self.initialize_perf()
+
+    def initialize_perf(self):
+        self.perf["store"] = []
+        self.perf["load"] = []
+        self.perf["op"] = []
+        self.perf["ai"] = []
+        self.perf["perf"] = []
 
     def get_info(self,*vcnt):
         """
         PackedFunc
         Do not call this function directly!
         """
-        self.perf["store"] = vcnt[0]
-        self.perf["load"] = vcnt[1]
-        self.perf["op"] = vcnt[2]
-        self.perf["ai"] = float(self.perf["op"]) / float(self.perf["store"] + self.perf["load"]) # arithmetic intensity
-        print("Store + Load: {} B + {} B = {} B".format(self.perf["store"],self.perf["load"],self.perf["store"] + self.perf["load"]))
-        print("# of ops: {} GFLOS".format(self.perf["op"] / 10**9))
-        print("Arithmetic density: {} FLOPs/Byte".format(self.perf["ai"]))
+        self.perf["store"].append(vcnt[0])
+        self.perf["load"].append(vcnt[1])
+        self.perf["op"].append(vcnt[2])
+        self.perf["ai"].append(float(self.perf["op"][-1]) / float(self.perf["store"][-1] + self.perf["load"][-1])) # arithmetic intensity
+        print("Store + Load: {} B + {} B = {} B".format(self.perf["store"][-1],self.perf["load"][-1],self.perf["store"][-1] + self.perf["load"][-1]))
+        print("# of ops: {} GFLOS".format(self.perf["op"][-1] / 10**9))
+        print("Arithmetic density: {} FLOPs/Byte".format(self.perf["ai"][-1]))
         print("Ridge point: {} FLOPs/Byte".format(self.ridge_point))
         print("I/O bandwidth roof: {:.2f} GB/s".format(self.bandwidth_roof/10**9))
         print("Compute roof: {:.2f} GFLOP/s".format(self.compute_roof/10**9))
-        if self.ridge_point > self.perf["ai"]:
+        if self.ridge_point > self.perf["ai"][-1]:
             print("Memory bound!")
         else:
             print("Computation bound!")
@@ -49,8 +58,8 @@ class Profiler():
         else:
             report = parse_xml("project")
         latency = report["PerformanceEstimates"]["SummaryOfOverallLatency"]["Best-caseLatency"]
-        self.perf["perf"] = self.perf["op"] / int(latency) * self.clock # FLOP/cycles * cycles/s -> FLOP/s
-        print("Real performance: {} GFLOP/s".format(self.perf["perf"]/(10**9)))
+        self.perf["perf"].append(self.perf["op"][-1] / int(latency) * self.clock) # FLOP/cycles * cycles/s -> FLOP/s
+        print("Real performance: {} GFLOP/s".format(self.perf["perf"][-1]/(10**9)))
 
     def roofline(self,log_plot=True,filename="roofline.png"):
         """
@@ -68,9 +77,10 @@ class Profiler():
             plt.xscale("log")
             plt.yscale("log")
         plt.plot(x, y)
-        plt.plot([self.perf["ai"]],[min(self.perf["ai"]*self.bandwidth_roof, self.compute_roof)],"x",color="orange",label="Attainable Performance")
-        plt.plot([self.perf["ai"]],[self.perf["perf"]],"o",color="red",label="Real Performance")
-        plt.vlines(x=self.perf["ai"], ymin=0, ymax=min(self.perf["ai"] * self.bandwidth_roof, self.compute_roof), linestyle="--",color="orange")
+        plt.plot(self.perf["ai"], np.array(self.perf["ai"])*self.bandwidth_roof,"x",color="orange",label="Attainable Performance")
+        plt.plot(self.perf["ai"], self.perf["perf"],"o",color="red",label="Real Performance")
+        for i in range(len(self.perf["ai"])):
+            plt.vlines(x=self.perf["ai"][i], ymin=0, ymax=min(self.perf["ai"][i] * self.bandwidth_roof, self.compute_roof), linestyle="--",color="orange")
         ax.set_axisbelow(True)
         ax.yaxis.grid(color='gray', linestyle='dashed')
         plt.title("Roofline Model")
